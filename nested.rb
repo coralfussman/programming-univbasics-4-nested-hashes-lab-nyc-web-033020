0e41d4a1c0f4c986862b6daca12a85387a7b6584
Coral Fussman <coralfussman@gmail.com>
2020-03-18 03:16:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:known_for][:languages]
+language = programmer_hash[:dennis_ritchie][:known_for][:languages]
+puts language
 end
 
 def adding_matz
